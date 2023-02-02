424ab732b128acbe69dc79220113b1c77a44bd07
webhero-nur <nurealom522@gmail.com>
2023-02-03 00:45:45 +0600

    load countries using useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+
+function LoadCountries() {
+  const [countris, setCountries] = useState([])
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+
+
+  }, [])
+
+  return (
+    <div>
+      <h1>Visiting Every country of the world</h1>
+      <h3>Available Countries: {countris.length}</h3>
+    </div>
+  )
 }
 
 export default App;
