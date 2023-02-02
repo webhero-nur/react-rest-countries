04fc791a4bb09c1f88d1c817628eaa7b91adea2b
webhero-nur <nurealom522@gmail.com>
2023-02-03 01:44:45 +0600

    Components inside the folder structure and connect then

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
 import Countries from './components/Countries/Countries';
-import Person from './components/Person/Person';
-import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
       <Countries></Countries>
-      <Person></Person>
     </div>
   );
 }
--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([])
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
     return (
         <div>
-            <h2>Countries in other file.</h2>
+            <h2>Hello From Countries.js</h2>
+            <p>Count: {countries.length}</p>
+            {
+                countries.map(country => console.log(country))
+            }
         </div>
     );
 };
